330347e0548a31cb34b133d223cf5076876460d5
Donald Highe <dhighe25@gmail.com>
2016-12-03 16:28:59 -0500

    added framework for how I want the flow of the website to work

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log('Everything is fine')
+
+$(document).ready(function($) {
+  $('#about-container').hide();
+  $('#project-container').hide();
+  $('#contact-container').hide();
+  setTimeout(function(){
+    $('#background-test').toggleClass('active');
+  },100);
+});
+
+$(function() {
+const header = $('#header-container');
+const homeButton = $('#home-btn');
+const aboutMe = $('#about-container');
+const aboutButton = $('#about-btn');
+const project = $('#project-container');
+const projectButton = $('#project-btn');
+const contact = $('#contact-container');
+const contactButton = $('#contact-btn');
+
+  homeButton.on('click', function(){
+    console.log("Main Button was clicked");
+    header.toggleClass('hide show');
+  });
+
+  aboutButton.on('click', function(){
+    aboutMe.show();
+    console.log("About Button was clicked")
+    aboutMe.toggleClass('hide show');
+
+  });
+
+  projectButton.on('click', function(){
+    project.show();
+    console.log("Project Button was clicked")
+    project.toggleClass('hide show');
+  });
+
+  contactButton.on('click', function(){
+    contact.show();
+    console.log("Contact Button was clicked")
+    contact.toggleClass('hide show');
+  });
+});
