a09edeb261409be1b185e8c02b8e4a50687a5f00
Donald Highe <dhighe25@gmail.com>
2017-01-03 09:53:03 -0700

    Testing a new php file

---
+++ contactForm.php
@@ -0,0 +1,38 @@
+<?php
+
+$EmailTo = "dhighe25@gmail.com";
+$Subject = "You have been contacted!";
+$Name = Trim(stripslashes($_POST['name']));
+$Email = Trim(stripslashes($_POST['email']));
+$Message = Trim(stripslashes($_POST['message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body);
+
+// redirect to success page
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ thanksForm.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+  <div>
+
+    <br /><br />
+
+    <h1>Your message has been sent!</h1><br />
+
+    <p><a href="index.html">back to website</a></p>
+
+  </div>
+
+  <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+  _uacct = "UA-68528-29";
+  urchinTracker();
+  </script>
+
+</body>
+
+</html>
